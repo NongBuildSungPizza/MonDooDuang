845f8380e939e11924a4867914daf783d74a2b16
IamMrTeapot <87607928+IamMrTeapot@users.noreply.github.com>
2024-01-25 21:23:51 +0700

    feat: add and setup redux

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,16 @@
 import "./App.css"
 import { BrowserRouter } from "react-router-dom"
 import Router from "./routes/router"
+import { Provider } from "react-redux"
+import store from "./redux/store"
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </Provider>
   )
 }
 
